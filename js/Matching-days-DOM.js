f629b3d948bde493bfa654d2fce51ba4489ccf73
Mr Booi <ayabongabooi2@gmail.com>
2018-05-07 14:23:51 +0200

    working Matching day code with on style

--- js/Matching-days-DOM.js
+++ js/Matching-days-DOM.js
@@ -1,130 +1,38 @@
 var First_date = document.querySelector('.first');
 var Second_date = document.querySelector('.second');
-
+//template
 var templateSource = document.querySelector(".MatchingDayTemplate").innerHTML;
-// compile the template
 var MatchingTemplate = Handlebars.compile(templateSource);
 var MatchingDayElem = document.querySelector('.wrapper');
 
-
-
 window.addEventListener('load', function() {
-  console.log(matchingDays.getWeekDay)
-
-  var dateData = {}
-  for (let i = 0; i < matchingDays.getWeekDay.length; i++) {
-    let current = matchingDays.getWeekDay[i];
-    console.log(current);
+   var dateData = {}
+  for (let i = 0; i < matchingDays.getWeekDays.length; i++) {
+    let current = matchingDays.getWeekDays[i];
     dateData[current] = {
-      dayId: matchingDays.getWeekDay[i],
-      dayName: matchingDays.getWeekDay[i],
-      first: false,
-      second: false
+      dayId: matchingDays.getWeekDays[i],
+      dayName: matchingDays.getWeekDays[i]
     }
   }
   var dateDataHTML = MatchingTemplate({weekdays:dateData});
   MatchingDayElem.innerHTML = dateDataHTML;
 });
 
-
-
-
-
-
-function MatchingDay() {
-  var dateOne = '';
-  var dateTwo = '';
-  const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-  function setDate1(value) {
-    dateOne = value;
-    return dateOne;
-  }
-
-  function setDate2(value) {
-    dateTwo = value;
-    return dateTwo;
-  }
-
-  function getDate1() {
-    return dateOne;
-  }
-
-  function getDate2() {
-    return dateTwo;
-  }
-
-  function sameDay(date1, date2) {
-    var pos = new Date(date1).getDay();
-    var poss = new Date(date2).getDay();
-    if (date1 != '' && date2 != '') {
-      return weekday[pos] == weekday[poss];
-    }
-  }
-
-  function week(value) {
-    let pos = new Date(value).getDay();
-    return weekday[pos];
-  }
-
-
-  return {
-    date1: setDate1,
-    date2: setDate2,
-    getDay1: getDate1,
-    getDay2: getDate2,
-    getweek: week,
-    getWeekDay: weekday,
-    sameDays: sameDay
-  }
-
-}
-
+//  of a  factory Function
 var matchingDays = MatchingDay();
-
 First_date.addEventListener("change", function() {
-  var day1 = document.querySelectorAll('.box');
-  let fdate = matchingDays.date1(First_date.value)
-  var firstDate = matchingDays.getweek(fdate);
-  var getsecond = matchingDays.getDay2();
-
-  var weekdays = {};
-
-  for (var i = 0; i < day1.length; i++) {
-    let currentPos = day1[i].innerHTML;
-     weekdays[currentPos] = {
-      dayId: currentPos,
-      dayName: currentPos
-    }
-    if (currentPos === firstDate) {
-      day1[i].classList.add("orange");
-      weekdays[currentPos] = Object.assign({
-        'first': 'orange'
-      }, weekdays[currentPos]);
-
-      if (matchingDays.sameDays(matchingDays.date1(First_date.value), matchingDays.getDay2())) {
-      
-      }
-    }
-  }
-  var dateData = {
-    weekdays
-  }
-  MatchingDayElem.innerHTML = MatchingTemplate(dateData);
+let store= matchingDays.weekdays(First_date.value,Second_date.value);
+  MatchingDayElem.innerHTML = MatchingTemplate({weekdays:store});
 });
 
 
 Second_date.addEventListener("change", function() {
-  var day2 = document.querySelectorAll('.box');
-  for (var i = 0; i < day2.length; i++) {
-    day2[i].classList.remove("blue", "green");
-    if (day2[i].innerHTML == matchingDays.getweek(Second_date.value)) {
-      day2[i].classList.add("blue");
-      if (matchingDays.sameDays(matchingDays.getDay1(), matchingDays.date2(Second_date.value))) {
-        day2[i].classList.remove("blue", "orange");
-        day2[i].classList.add("green");
-      }
-    }
 
-  }
+  let store= matchingDays.weekdays(First_date.value,Second_date.value);
+
+    console.log({weekdays:store});
+
+ console.log(matchingDays.sameDays(First_date.value,Second_date.value));
+
+    MatchingDayElem.innerHTML = MatchingTemplate({weekdays:store});
 });
---
+++ js/Matching-days.js
@@ -0,0 +1,95 @@
+// factory function
+function MatchingDay() {
+    var dateOne = '';
+    var dateTwo = '';
+    const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    function setDate1(value) {
+      dateOne = value;
+      return dateOne;
+    }
+
+    function setDate2(value) {
+      dateTwo = value;
+      return dateTwo;
+    }
+
+    function getDate1() {
+      return dateOne;
+    }
+
+    function getDate2() {
+      return dateTwo;
+    }
+
+    function sameDay(date1, date2) {
+      var pos = new Date(date1).getDay();
+      var poss = new Date(date2).getDay();
+      if (date1 != undefined && date2 != undefined) {
+        return weekday[pos] == weekday[poss];
+      }
+    }
+
+    function week(value) {
+      if (value !==undefined) {
+        let pos = new Date(value).getDay();
+        return weekday[pos];
+      }
+
+
+    }
+
+    function weekdayLogic (firstDate,secondDate){
+      var weekdays = {};
+      for (var i = 0; i < weekday.length; i++) {
+        var currentPos = weekday[i];
+         weekdays[currentPos] = {
+          dayId: currentPos,
+          dayName: currentPos
+        }
+
+        if (currentPos === week(firstDate)) {
+          weekdays[currentPos] = Object.assign({
+            'first': 'orange'
+          }, weekdays[currentPos]);
+
+         if (sameDay(firstDate,secondDate)) {
+                 delete weekdays[currentPos].first;
+                    weekdays[currentPos] = Object.assign({
+                      'sameDay': 'green'
+                    }, weekdays[currentPos]);
+               }
+        }
+
+      else if (currentPos === week(secondDate)) {
+            weekdays[currentPos] = Object.assign({
+              'second': 'blue'
+            }, weekdays[currentPos]);
+
+            if (sameDay(firstDate,secondDate)) {
+                    delete weekdays[currentPos].second;
+                       weekdays[currentPos] = Object.assign({
+                         'sameDay': 'green'
+                       }, weekdays[currentPos]);
+                  }
+        }
+
+
+
+}
+  return weekdays;
+    }
+
+    return {
+      date1: setDate1,
+      date2: setDate2,
+      getDay1: getDate1,
+      getDay2: getDate2,
+      getweek: week,
+      getWeekDays: weekday,
+      sameDays: sameDay,
+      weekdays :weekdayLogic
+
+    }
+
+  }
